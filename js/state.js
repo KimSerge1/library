11d5823af8bf0ccc12a5d51180f02276b971010c
KimSerge1 <07.selan40@gmail.com>
2019-04-20 03:32:31 +0300

    Create sothing

---
+++ js/state.js
@@ -0,0 +1,36 @@
+let state={
+    autors:[{
+        autor:'Agata Cristy',
+        book: 'Убийство в восточном экспрессе',
+        isbn:'123456',
+        discription: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint minima doloremque fugiat dolores non a reiciendis numquam, mollitia voluptas provident explicabo nostrum omnis. Veniam, omnis corrupti. Quod molestiae facilis distinctio.',
+        categoryes:['детектив','криминал','роман']
+    },{
+        autor:'Анхель де Куатье',
+        book:'Дневники сумашедшего',
+        isbn:123123,
+        discription:'Про психа',
+        categoryes:['детектив','роман','психология']
+    },{
+        autor:'Дениел Киз',
+        book:'Цветы для Элджернона',
+        isbn:123345,
+        discription:'Очень интересная книжка, прочитал на одном дыхании',
+        categoryes:['роман','зарубежная проза','психология']
+    },{
+        autor:'Анхель де Куатье',
+        book:'Дневники сумашедшего',
+        isbn:123123,
+        discription:'Про психа',
+        categoryes:['детектив','роман','психология']
+    },{
+        autor:'Анхель де Куатье',
+        book:'Дневники самурая',
+        isbn:123123,
+        discription:'Про психа',
+        categoryes:['детектив','роман','психология']
+    }],
+    categotyes:[
+        
+    ]
+};
